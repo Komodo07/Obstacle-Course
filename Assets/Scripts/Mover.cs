196631d786eb2466a6d3b16e5c44b92fca766c80
Jason Tucker <komodo.07@outlook.com>
2023-02-15 15:04:51 -0600

    Added movement
    Added movement to the Mover script and added color materials to the UFO and ground plane objects.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
     }
 }
