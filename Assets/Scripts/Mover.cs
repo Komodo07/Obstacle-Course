6ae2482f3aa3e4bbb15ed54d0ca824d22829635a
Jason Tucker <komodo.07@outlook.com>
2023-02-16 16:05:45 -0600

    Updated mover.cs
    Updated mover script to add console output and refactored player movement to a method.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,15 +9,27 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, 0, zValue);
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move the player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
     }
 }
