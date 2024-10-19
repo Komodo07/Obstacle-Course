566d77c7a5093f231232f33f97d8cd57f492cf5c
Jason Tucker <komodo.07@outlook.com>
2024-10-19 13:47:53 -0500

    Updated mover script
    Updated mover script to react to wasd key presses

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -3,21 +3,18 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0f;
-    float yValue = 0.01f;
-    float zValue = 0f;    
-    
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(xValue, yValue, zValue);
-        transform.Translate(movement * Time.deltaTime * 10);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+
+        transform.Translate(xValue, 0, zValue);
     }
 }
