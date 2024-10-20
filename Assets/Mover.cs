732446b4845fa73669252f4656346be3b13e57cf
Jason Tucker <komodo.07@outlook.com>
2024-10-20 10:30:07 -0500

    Added Player Camera
    Added a player camera to follow the player around. Also added input to the mover script so player can move the object.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    float moveSpeed = 10.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,8 +15,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, 0, zValue);
     }
